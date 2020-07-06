afd4a924df33d2472bff87022c5ffc5ec1407e8a
Ivan Vergés <ivan@platoniq.net>
2020-07-06 16:36:34 +0200

    fix reply email

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -2,7 +2,7 @@
 
 Decidim.configure do |config|
   config.application_name = "[TEST] Participa Catalunya En Comú"
-  config.mailer_sender = "norely@catalunyaencomu.cat"
+  config.mailer_sender = "noreply@testparticipa.catalunyaencomu.cat"
 
   # Change these lines to set your preferred locales
   config.default_locale = :ca
