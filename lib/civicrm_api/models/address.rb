07a05843a16670030319ef1da34d3085ee091d74
Vera Rojman <vrojman@protonmail.com>
2020-10-13 10:53:57 +0200

    Use all civicrm user info on login

---
+++ lib/civicrm_api/models/address.rb
@@ -0,0 +1,21 @@
+module CivicrmApi
+  module Models
+    module Address
+      class << self
+        def from_contact(json)
+          {
+            country: json["country"],
+            city: json["city"],
+            postal_code: json["postal_code"],
+            street_address: json["street_address"],
+            state_province_name: json["state_province_name"],
+            state_province: json["state_province"],
+            supplemental_address_1: json["supplemental_address_1"],
+            supplemental_address_2: json["supplemental_address_2"],
+            supplemental_address_3: json["supplemental_address_3"],
+          }
+        end
+      end
+    end
+  end
+end
