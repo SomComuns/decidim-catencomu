e851a61141993253fbaffe7ced90d951132b7715
Vera Rojman <vrojman@protonmail.com>
2020-10-14 12:15:44 +0200

    Move error class to separate file

--- lib/civicrm_api/request.rb
+++ lib/civicrm_api/request.rb
@@ -46,12 +46,4 @@ module CivicrmApi
 
     attr_reader :response
   end
-
-  class Error < StandardError
-    def initialize(detail = nil)
-      msg = "There was an error with the CiViCRM API"
-      msg += " (#{detail})" if detail.present?
-      super(msg)
-    end
-  end
 end
