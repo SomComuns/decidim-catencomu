ca84de78b66970fcdccdfd3a6c0b1e0d4439d509
David Igón <david.igon@riseup.net>
2020-11-04 17:41:06 +0100

    recover attribute :role in civicrm verification workflow

--- config/initializers/civicrm_verification_workflow.rb
+++ config/initializers/civicrm_verification_workflow.rb
@@ -4,6 +4,7 @@ Decidim::Verifications.register_workflow(:civicrm) do |workflow|
   workflow.form = "Decidim::Verifications::Civicrm"
 
   workflow.options do |options|
+    options.attribute :role, type: :enum, choices: -> { Decidim::Civicrm::Api::User::ROLES.values.map(&:to_s) }
     options.attribute :regional_scope, type: :enum, choices: -> { Decidim::Civicrm::Api::RegionalScope::ALL.keys.map(&:to_s) }
   end
 end
