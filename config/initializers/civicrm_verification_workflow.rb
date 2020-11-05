a860f2b7fffb0bc62ca2d54766e8f39257fa20b6
Vera Rojman <vrojman@protonmail.com>
2020-11-05 13:47:31 +0100

    WIP on create verification engine

--- config/initializers/civicrm_verification_workflow.rb
+++ config/initializers/civicrm_verification_workflow.rb
@@ -2,9 +2,16 @@
 
 Decidim::Verifications.register_workflow(:civicrm) do |workflow|
   workflow.form = "Decidim::Verifications::Civicrm"
-
+  
   workflow.options do |options|
     options.attribute :role, type: :enum, choices: -> { Decidim::Civicrm::Api::User::ROLES.values.map(&:to_s) }
     options.attribute :regional_scope, type: :enum, choices: -> { Decidim::Civicrm::Api::RegionalScope::ALL.keys.map(&:to_s) }
   end
+end
+
+require "decidim/verifications/civicrm_groups"
+
+Decidim::Verifications.register_workflow(:civicrm_groups) do |workflow|
+  workflow.engine = Decidim::Verifications::CivicrmGroups::Engine
+  workflow.admin_engine = Decidim::Verifications::CivicrmGroups::AdminEngine
 end
