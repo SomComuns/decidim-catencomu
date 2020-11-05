08ca5c3d16467462eedf9e646cec9614c2c3a03d
Vera Rojman <vrojman@protonmail.com>
2020-11-05 13:53:42 +0100

    Fix rubocop offenses

--- lib/decidim/verifications/civicrm_groups.rb
+++ lib/decidim/verifications/civicrm_groups.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "decidim/verifications/civicrm_groups/admin"
 require "decidim/verifications/civicrm_groups/admin_engine"
 require "decidim/verifications/civicrm_groups/engine"
