2036fd7fb0b03eaa2d68b4587b3347c62a57a85c
Vera Rojman <vrojman@protonmail.com>
2020-11-17 16:41:18 +0100

    List groups in admin section

---
+++ lib/decidim/civicrm/api/group.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Civicrm
+    module Api
+      module Group
+        class << self
+          def parse_groups(json)
+            json.map { |group| parse_group(group) }
+          end
+
+          def parse_group(json)
+            {
+              id: json["id"],
+              name: json["name"],
+              description: json["description"],
+              visibility: json["visibility"],
+              group_type: json["group_type"],
+              is_active: json["is_active"],
+              is_hidden: json["is_hidden"],
+              is_reserved: json["is_reserved"]
+            }
+          end
+        end
+      end
+    end
+  end
+end
