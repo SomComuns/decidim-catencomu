9c854de85d408ce70a9e5c903f8716be7687f98d
Ivan Vergés <ivan@platoniq.net>
2020-12-28 21:56:21 +0100

    remove custom mentu test

--- config/initializers/custom_menus.rb
+++
@@ -1,8 +0,0 @@
-
-Rails.application.config.after_initialize do
- Decidim::MenuRegistry.find(:menu).configurations[0] = Proc.new do |menu|
-    menu.item "test #{Decidim::MenuRegistry.find(:menu).configurations.count}",
-            decidim.root_path,
-            position: 1.1
-  end  
-end
