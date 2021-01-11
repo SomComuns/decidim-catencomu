4533597acf1615d9f91bed61d43197913c42d623
Vera Rojman <vrojman@protonmail.com>
2021-01-11 14:02:02 +0100

    Add example for groups request

--- spec/lib/api/request_spec.rb
+++ spec/lib/api/request_spec.rb
@@ -10,12 +10,12 @@ describe "Decidim::Civicrm::Api::Request", type: :class do
       context "and with_contact option is false" do
         it "returns a valid user" do
           stub_user_valid_request
-          expect(subject.get_user(42, with_contact: false)).to eq({
+          expect(subject.get_user(42, with_contact: false)).to eq(
             "id" => "42",
             "name" => "arthur.dent",
             "email" => "arthurdent@example.com",
             "contact_id" => "9999"
-          })
+          )
         end
       end
 
@@ -23,15 +23,15 @@ describe "Decidim::Civicrm::Api::Request", type: :class do
         it "returns a valid user" do
           stub_user_valid_request
           stub_contact_valid_request
-          expect(subject.get_user(42, with_contact: true)).to eq({
+          expect(subject.get_user(42, with_contact: true)).to eq(
             "id" => "9999",
             "name" => "arthur.dent",
             "display_name" => "Sir Arthur Dent",
             "email" => "arthurdent@example.com",
             "contact_id" => "9999",
-            "roles" => {"2"=>"authenticateduser", "3"=>"administrator"},
-            "api.Address.get" => {"count"=>1, "id"=>888, "is_error"=>0, "values"=>[{"custom_23"=>"AT012", "id"=>"888"}], "version"=>3}
-          })
+            "roles" => { "2" => "authenticateduser", "3" => "administrator" },
+            "api.Address.get" => { "count" => 1, "id" => 888, "is_error" => 0, "values" => [{ "custom_23" => "AT012", "id" => "888" }], "version" => 3 }
+          )
         end
       end
     end
@@ -47,6 +47,40 @@ describe "Decidim::Civicrm::Api::Request", type: :class do
       it "raises an error" do
         stub_user_invalid_request
         expect { subject.get_user(42, with_contact: false) }.to raise_error Decidim::Civicrm::Api::Error
+      end
+    end
+  end
+
+  describe "#fetch_groups" do
+    context "when request is successful" do
+      it "returns parsed groups" do
+        stub_groups_valid_request
+        expect(subject.fetch_groups).to eq([
+                                             {
+                                               id: "1",
+                                               name: "Administrators",
+                                               title: "Administrators",
+                                               description: "The users in this group are assigned admin privileges.",
+                                               is_active: "1",
+                                               visibility: "User and User Admin Only",
+                                               group_type: [
+                                                 "1"
+                                               ],
+                                               is_hidden: "0",
+                                               is_reserved: "0"
+                                             },
+                                             {
+                                               id: "2",
+                                               name: "Another_Group",
+                                               title: "Another Group",
+                                               description: "...",
+                                               is_active: "1",
+                                               visibility: "User and User Admin Only",
+                                               group_type: "2",
+                                               is_hidden: "0",
+                                               is_reserved: "0"
+                                             }
+                                           ])
       end
     end
   end
--- spec/support/civicrm_stubs.rb
+++ spec/support/civicrm_stubs.rb
@@ -47,6 +47,18 @@ module CivicrmStubs
       )
   end
 
+  ## Groups
+  def stub_groups_valid_request
+    stub_request(:get, groups_request_url)
+      .with(
+        headers: headers
+      ).to_return(
+        status: 200,
+        body: file_fixture("groups_valid_response.txt").read,
+        headers: {}
+      )
+  end
+
   private
 
   def user_request_url
@@ -57,12 +69,16 @@ module CivicrmStubs
     "https://api.base/?action=Get&api_key=api-key&contact_id=9999&entity=Contact&json=%7B%22sequential%22:1,%22return%22:%22roles,display_name%22,%22api.Address.get%22:%7B%22return%22:%22custom_23%22%7D%7D&key=secret"
   end
 
+  def groups_request_url
+    "https://api.base/?action=Get&api_key=api-key&entity=Group&json=%7B%22sequential%22:1,%22options%22:%7B%22limit%22:0%7D,%22return%22:%22id,%20name,%20title,%20description,%20group_type,%20visibility%22,%22is_active%22:true%7D&key=secret"
+  end
+
   # This will change depending on your gems versions.
   def headers
     {
       "Accept" => "*/*",
-      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-      'User-Agent'=>'Faraday v1.2.0'
+      "Accept-Encoding" => "gzip;q=1.0,deflate;q=0.6,identity;q=0.3",
+      "User-Agent" => "Faraday v1.2.0"
     }
   end
 end
