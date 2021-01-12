bcc7227cf157f37e117cac4a17449d664d99c104
Vera Rojman <vera@platoniq.net>
2021-01-12 14:50:30 +0100

    Update to platoniq/decidim:temp/0.23-stable (#46)
    * Update decidim-civicrm gem
    
    * Update to temp/0.23-stable
    
    * Add migrations
    
    * Update schema
    
    * Update faraday version in stub helper
    
    * Fix proposals spec
    
    * Uncomment authorizable vote_comment action spec
    
    * Update schema
    
    * Remove overriden files from 30c9bfd fixing terms and conditions

--- spec/system/proposals_spec.rb
+++ spec/system/proposals_spec.rb
@@ -11,12 +11,12 @@ describe "Visit a proposal", type: :system, perform_enqueued: true do
   before do
     switch_to_host(organization.host)
     page.visit main_component_path(proposals_component)
-    click_link proposal.title
+    click_link proposal.title["en"]
   end
 
   it "allows viewing a single proposal" do
-    expect(page).to have_content(proposal.title)
-    expect(page).to have_content(strip_tags(proposal.body).strip)
+    expect(page).to have_content(proposal.title["en"])
+    expect(page).to have_content(strip_tags(proposal.body["en"]).strip)
   end
 
   context "when has markdown" do
