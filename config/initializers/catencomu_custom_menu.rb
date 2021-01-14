e8551c26496996bb903f90f33aa2f88117709bdd
Vera Rojman <vrojman@protonmail.com>
2021-01-14 18:16:16 +0100

    Link 'Home' menu item to custom url

---
+++ config/initializers/catencomu_custom_menu.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+Rails.application.config.after_initialize do
+  Decidim::MenuRegistry.find(:menu).configurations[0] = proc do |menu|
+    menu.item I18n.t("menu.home", scope: "decidim"),
+              Rails.application.secrets.home_url || decidim.root_path,
+              position: 1
+  end
+end
