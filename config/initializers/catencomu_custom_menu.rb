a74d1ab7d58768a25c815eb05204f374703dda6f
Vera Rojman <vera@platoniq.net>
2021-02-05 10:33:09 +0100

    Fix active menu item (#55)
    * Millores varies (#49)
    
    * Change 'Les meves dades' to 'Dades de participació'
    
    * Add callout to account page
    
    * Link 'Home' menu item to custom url
    
    * Add link to participacio in account page
    
    * Add link to external sign_up in login page
    
    * Localize external urls when possible
    
    * Add missing translation
    
    * Fix active item in menu

--- config/initializers/catencomu_custom_menu.rb
+++ config/initializers/catencomu_custom_menu.rb
@@ -4,6 +4,7 @@ Rails.application.config.after_initialize do
   Decidim::MenuRegistry.find(:menu).configurations[0] = proc do |menu|
     menu.item I18n.t("menu.home", scope: "decidim"),
               Rails.application.secrets.home_url || decidim.root_path,
-              position: 1
+              position: 1,
+              active: ["decidim/homepage" => :show]
   end
 end
