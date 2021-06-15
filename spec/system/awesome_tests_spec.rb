02e9b637e8c52a5c4473e2ef8cc400965c90c603
Ivan Vergés <ivan@platoniq.net>
2021-06-15 12:11:11 +0200

    fix awesome specs

--- spec/system/awesome_tests_spec.rb
+++ spec/system/awesome_tests_spec.rb
@@ -83,7 +83,7 @@ describe "Custom styles", type: :system do
 
       context "and page matches the scope" do
         before do
-          click_link "Processos"
+          click_link "Processes"
         end
 
         it_behaves_like "extra css is added"
