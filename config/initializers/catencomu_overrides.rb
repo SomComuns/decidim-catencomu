9a536989fa51a4e7bf59574cc873280c182636f9
Ivan Vergés <ivan@platoniq.net>
2021-06-16 17:34:14 +0200

    duplicate processes: separate non-grouped

--- config/initializers/catencomu_overrides.rb
+++ config/initializers/catencomu_overrides.rb
@@ -1,5 +1,35 @@
 # frozen_string_literal: true
 
+# check application.rb to see the middleware initialization
+
 Rails.application.config.to_prepare do
+  # tweak authentication for closed organizations
   Decidim::ForceAuthentication.include(Decidim::ForceAuthenticationOverride)
+  # separate participatory processes in 2 menus (within or without a process group)
+  Decidim::ParticipatoryProcess.include(Decidim::ParticipatoryProcessOverride)
+  Decidim::ParticipatoryProcesses::ProcessFiltersCell.include(Decidim::ParticipatoryProcesses::ProcessFiltersCellOverride)
+end
+
+Rails.application.config.after_initialize do
+  # Creates a new menu next to Processes for grouped processes
+  if Rails.application.secrets.scope_ungrouped_processes[:enabled]
+    Decidim.menu :menu do |menu|
+      path = ParticipatoryProcessesScoper::ALTERNATIVE_NAMESPACE
+      key = Rails.application.secrets.scope_ungrouped_processes[:key] || path
+      position = Rails.application.secrets.scope_ungrouped_processes[:position_in_menu]
+
+      menu.item I18n.t(key, scope: "decidim.scope_ungrouped_processes"),
+                Rails.application.routes.url_helpers.send("#{path}_path"),
+                position: position,
+                if: (
+                  Decidim::ParticipatoryProcess
+                  .unscoped
+                  .where(organization: current_organization)
+                  .where.not(decidim_participatory_process_group_id: nil)
+                  .published
+                  .any?
+                ),
+                active: :inclusive
+    end
+  end
 end
