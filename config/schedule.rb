ef4072a70b2f72735bc22303f235db24c37b2158
Vera Rojman <vera@platoniq.net>
2021-07-26 18:11:49 +0200

    Add whenever and task config (#77)
    * Add whenever and task config
    
    * Lint

---
+++ config/schedule.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+# Example:
+#
+# set :output, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+# Learn more: http://github.com/javan/whenever
+
+every 1.day do
+  rake "civicrm:create_private_process_for_groups[2]" # parameter '2' is the organization_id
+end
