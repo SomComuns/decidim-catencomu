cb3895a664dfafcccd405efc039cf597e39d3a85
Ivan Vergés <ivan@platoniq.net>
2022-01-14 12:50:39 +0100

    manual link

--- catcomu-managers/lib/catcomu/managers.rb
+++ catcomu-managers/lib/catcomu/managers.rb
@@ -5,5 +5,10 @@ require "catcomu/managers/admin_engine"
 
 module Catcomu
   module Managers
+    include ActiveSupport::Configurable
+
+    config_accessor :manual_link do
+      "https://raw.githubusercontent.com/Platoniq/decidim-catencomu/main/catcomu-managers/docs/managers-manual.pdf"
+    end
   end
 end
