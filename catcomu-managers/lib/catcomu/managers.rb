c2f741e7041b0e6ed86c05850c4ead27d82a3809
Ivan Vergés <ivan@platoniq.net>
2022-01-21 12:30:47 +0100

    add manual

--- catcomu-managers/lib/catcomu/managers.rb
+++ catcomu-managers/lib/catcomu/managers.rb
@@ -8,7 +8,11 @@ module Catcomu
     include ActiveSupport::Configurable
 
     config_accessor :manual_link do
-      "https://raw.githubusercontent.com/Platoniq/decidim-catencomu/main/catcomu-managers/docs/managers-manual.pdf"
+      "https://raw.githubusercontent.com/Platoniq/decidim-catencomu/main/catcomu-managers/docs/manual-cat_v1.1.pdf"
+    end
+
+    config_accessor :contact_data do
+      ""
     end
   end
 end
