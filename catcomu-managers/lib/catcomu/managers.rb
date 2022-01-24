83870785426629f2a6d30cc5821242f6f330c49f
Ivan Vergés <ivan@platoniq.net>
2022-01-24 14:20:41 +0100

    set default groups for managers (#100)
    * set default groups for managers
    
    * let admins choose civicrm sync groups
    
    * add workers
    
    * update module
    
    * set defaults civicrm groups for superadmins too
    
    * texts
    
    * Update catcomu-managers/config/locales/ca.yml
    
    Co-authored-by: marta-platoniq <72607737+marta-platoniq@users.noreply.github.com>
    
    * Update catcomu-managers/config/locales/ca.yml
    
    Co-authored-by: marta-platoniq <72607737+marta-platoniq@users.noreply.github.com>
    
    * spells
    
    * spells
    
    * manual link
    
    * add migrations
    
    * fix secrets
    
    * fix tests
    
    * Update catcomu-managers/config/locales/ca.yml
    
    Co-authored-by: marta-platoniq <72607737+marta-platoniq@users.noreply.github.com>
    
    * Update catcomu-managers/config/locales/es.yml
    
    Co-authored-by: marta-platoniq <72607737+marta-platoniq@users.noreply.github.com>
    
    * add manual
    
    * add manual
    
    * remove console
    
    Co-authored-by: marta-platoniq <72607737+marta-platoniq@users.noreply.github.com>

--- catcomu-managers/lib/catcomu/managers.rb
+++ catcomu-managers/lib/catcomu/managers.rb
@@ -5,5 +5,14 @@ require "catcomu/managers/admin_engine"
 
 module Catcomu
   module Managers
+    include ActiveSupport::Configurable
+
+    config_accessor :manual_link do
+      "https://raw.githubusercontent.com/Platoniq/decidim-catencomu/main/catcomu-managers/docs/manual-cat_v1.1.pdf"
+    end
+
+    config_accessor :contact_data do
+      ""
+    end
   end
 end
