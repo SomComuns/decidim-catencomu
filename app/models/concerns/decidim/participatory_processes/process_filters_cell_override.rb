a242a24321c161c15a8e8d3ba1b29f35258352ad
Francisco Bolívar <francisco.bolivar@nazaries.com>
2024-01-11 11:46:38 +0100

    Fix overrides_spec.rb

--- app/models/concerns/decidim/participatory_processes/process_filters_cell_override.rb
+++ app/models/concerns/decidim/participatory_processes/process_filters_cell_override.rb
@@ -6,36 +6,30 @@ module Decidim
       extend ActiveSupport::Concern
 
       included do
-        def filter_link(filter)
-          current_participatory_processes_path(
-            filter: {
-              scope_id: get_filter(:scope_id),
-              area_id: get_filter(:area_id),
-              date: filter
-            }
-          )
+        def filter_link(date_filter, type_filter = nil)
+          current_participatory_processes_path(date_filter, type_filter)
         end
 
         private
 
         # use the alternate url for generating filters if we are scoped
-        def current_participatory_processes_path(filter)
+        def current_participatory_processes_path(date_filter, type_filter)
           if Decidim::ParticipatoryProcess.scoped_groups_namespace == ParticipatoryProcessesScoper::ALTERNATIVE_NAMESPACE
-            return alternative_filter_link(Decidim::ParticipatoryProcess.scoped_groups_namespace, filter)
+            return alternative_filter_link(Decidim::ParticipatoryProcess.scoped_groups_namespace, date_filter)
           end
 
-          normal_filter_link(filter)
+          normal_filter_link(date_filter, type_filter)
         end
 
         def alternative_filter_link(key, filter)
           Rails.application.routes.url_helpers.send("#{key}_path", filter)
         end
 
-        def normal_filter_link(filter)
+        def normal_filter_link(date_filter, type_filter)
           Decidim::ParticipatoryProcesses::Engine
             .routes
             .url_helpers
-            .participatory_processes_path(filter)
+            .participatory_processes_path(**filter_params(date_filter, type_filter))
         end
       end
     end
