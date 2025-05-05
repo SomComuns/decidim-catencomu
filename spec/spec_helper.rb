2ae41e217312858cf9b3742e0d74f6099a47f924
elviabth <ejbenedith@gmail.com>
2025-05-05 15:47:31 +0200

    move views to overrides folder

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,6 +14,9 @@
 # the additional setup, and require it from the spec files that actually need
 # it.
 #
+# The `.rspec` file also contains a few flags that are not defaults but that
+# users commonly want.
+#
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
@@ -54,16 +57,19 @@ RSpec.configure do |config|
   #   # aliases for `it`, `describe`, and `context` that include `:focus`
   #   # metadata: `fit`, `fdescribe` and `fcontext`, respectively.
   #   config.filter_run_when_matching :focus
+  #
   #   # Allows RSpec to persist some state between runs in order to support
   #   # the `--only-failures` and `--next-failure` CLI options. We recommend
   #   # you configure your source control system to ignore this file.
   #   config.example_status_persistence_file_path = "spec/examples.txt"
+  #
   #   # Limits the available syntax to the non-monkey patched syntax that is
   #   # recommended. For more details, see:
   #   #   - http://rspec.info/blog/2012/06/rspecs-new-expectation-syntax/
   #   #   - http://www.teaisaweso.me/blog/2013/05/27/rspecs-new-message-expectation-syntax/
   #   #   - http://rspec.info/blog/2014/05/notable-changes-in-rspec-3/#zero-monkey-patching-mode
   #   config.disable_monkey_patching!
+  #
   #   # Many RSpec users commonly either run the entire suite or an individual
   #   # file, and it's useful to allow more verbose output when running an
   #   # individual spec file.
@@ -71,17 +77,20 @@ RSpec.configure do |config|
   #     # Use the documentation formatter for detailed output,
   #     # unless a formatter has already been configured
   #     # (e.g. via a command-line flag).
-  #     config.default_formatter = "doc"
+  #     config.default_formatter = 'doc'
   #   end
+  #
   #   # Print the 10 slowest examples and example groups at the
   #   # end of the spec run, to help surface which specs are running
   #   # particularly slow.
   #   config.profile_examples = 10
+  #
   #   # Run specs in random order to surface order dependencies. If you find an
   #   # order dependency and want to debug it, you can fix the order by providing
   #   # the seed, which is printed after each run.
   #   #     --seed 1234
   #   config.order = :random
+  #
   #   # Seed global randomization in this process using the `--seed` CLI option.
   #   # Setting this allows you to use `--seed` to deterministically reproduce
   #   # test failures related to randomization by passing the same `--seed` value
@@ -90,13 +99,13 @@ RSpec.configure do |config|
   config.order = :random
 
   config.before do
-    I18n.available_locales = [:en, :ca, :es]
+    I18n.available_locales = [:en, :es, :ca]
     I18n.default_locale = :en
-    # rubocop:disable Rails/I18nLocaleAssignment
-    I18n.locale = :en
-    # rubocop:enable Rails/I18nLocaleAssignment
-    Decidim.available_locales = [:en, :ca, :es]
+    Decidim.available_locales = [:en, :es, :ca]
     Decidim.default_locale = :en
-    Capybara.server = :puma
+  end
+
+  config.around do |example|
+    I18n.with_locale(:en) { example.run }
   end
 end
