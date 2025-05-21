3a0c6fa800b03a7f6d8c7eee131d4693344ab17a
Ivan Vergés <ivan@pokecode.net>
2025-05-21 11:40:41 +0200

    update decidim

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,10 +1,18 @@
 # frozen_string_literal: true
 
+require "active_support/core_ext/integer/time"
+
+# The test environment is used exclusively to run your application's
+# test suite. You never need to work with it otherwise. Remember that
+# your test database is "scratch space" for the test suite and is wiped
+# and recreated between test runs. Don't rely on the data there!
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Turn false under Spring and add config.action_view.cache_template_loading = true.
-  config.cache_classes = true
+  config.cache_classes = false
+  config.action_view.cache_template_loading = true
 
   # Eager loading loads your whole application. When running a single test locally,
   # this probably isn't necessary. It's a good idea to do in a continuous integration
